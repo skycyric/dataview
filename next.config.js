ccff311961281335b6ca8b05aba20ed4b0c65c75
skycyric <skycyric@gmail.com>
2023-04-19 10:59:58 +0800

    commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 module.exports = {
   env: {
     API_KEY: '81aa3fc0ace24fdcbf5ebcf73f79a8ff',
   },
-  output: "export",
-  distDir: "./docs",
+  distDir: isDevelopment ? './dev' : './docs',
+  reactStrictMode: true,
 };
